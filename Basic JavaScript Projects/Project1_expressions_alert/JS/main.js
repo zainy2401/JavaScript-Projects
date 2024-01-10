dbbad7f6d3c45d63729e5d1e9db95041836d5289
zainy2401 <zainuaziz2401@outlook.com>
2024-01-10 13:56:04 +0000

    Basic JavaScript Project
    Created html and js file that demonstrates basic JS commands.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+window.alert("Hello World!"); //Produces a broswer pop up with message.
+document.write(" Hello World!"); //Writes text into document.
+
+var A = " This is a string."; //Assigns a string variable.
+document.write(A); //Prints the previous string variable to document.
+ 
+var pieceOfText = "This is a piece of text."; //Assigns a string variable.
+window.alert(pieceOfText); //Writes previous string to a window popup.
+
+document.write(" So I said \"Don't touch my Pizza\"."); //Writes a string to document that uses quotation marks.
+
+document.write(" Hello" + "\, again\," + " World."); //Writes a string that is concatenated to the document.
+
+var B = " My name is" + " Zain"; //Assigns a concatenated string to a variable.
+document.write(B); //Prints the variable that has concatenated strings to document.
+
+var fruit = " Apple", vegetable = " Potato", meat = " Chicken"; //Creates multiple string variables in one statement.
+var fruit = fruit.fontcolor("red"); //Assigns the variable a font colour of red.
+var vegetable = vegetable.fontcolor("green"); //Assigns the variable a font colour of green.
+var meat = meat.fontcolor("orange"); //Assigns the variable a font colour of orange.
+document.write(fruit); //Prints a variable to document.
+document.write(vegetable); //Prints a variable to document.
+document.write(meat); //Prints a variable to document.
+
+document.write(3 + 3); //Prints an expression to document.
